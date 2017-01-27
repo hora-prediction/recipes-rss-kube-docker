f3b19d3a647636abcc92856b55240e07a47ec5cf
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2017-01-27 15:35:53 +0100

    Fix bug that makes all requests use the same user id

--- locust/test/locustfile.py
+++ locust/test/locustfile.py
@@ -2,79 +2,78 @@ from locust import HttpLocust, TaskSet, task
 from random import randint
 
 class MyTaskSet(TaskSet):
-    rand_user = None
 
     def on_start(self):
-        self.rand_user = randint(1, 999999999)
-        print("username = ", self.rand_user)
+        self.user_id = randint(1,999999999)
+        print("username = ", self.user_id)
 
-    @task(10)
+    @task(1)
     def view(self):
         self.client.get("/jsp/rss.jsp")
 
     @task(1)
     def addAbc(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/abc.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/abc.xml", "username":self.user_id})
 
     @task(1)
     def addBbc(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/bbc.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/bbc.xml", "username":self.user_id})
 
     @task(1)
     def addCnn(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/cnn.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/cnn.xml", "username":self.user_id})
 
     @task(1)
     def addDw(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/dw.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/dw.xml", "username":self.user_id})
 
     @task(1)
     def addForbes(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/forbes.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/forbes.xml", "username":self.user_id})
 
     @task(1)
     def addReuters(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/reuters.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/reuters.xml", "username":self.user_id})
 
     @task(1)
     def addSpiegel(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/spiegel.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/spiegel.xml", "username":self.user_id})
 
     @task(1)
     def addWsj(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/wsj.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/wsj.xml", "username":self.user_id})
 
     @task(1)
     def deleteAbc(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/abc.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/abc.xml", "username":self.user_id})
 
     @task(1)
     def deleteBbc(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/bbc.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/bbc.xml", "username":self.user_id})
 
     @task(1)
     def deleteCnn(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/cnn.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/cnn.xml", "username":self.user_id})
 
     @task(1)
     def deleteDw(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/dw.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/dw.xml", "username":self.user_id})
 
     @task(1)
     def deleteForbes(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/forbes.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/forbes.xml", "username":self.user_id})
 
     @task(1)
     def deleteReuters(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/reuters.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/reuters.xml", "username":self.user_id})
 
     @task(1)
     def deleteSpiegel(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/spiegel.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/spiegel.xml", "username":self.user_id})
 
     @task(1)
     def deleteWsj(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/wsj.xml", "username":self.rand_user})
+        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/wsj.xml", "username":self.user_id})
 
 class MyLocust(HttpLocust):
     task_set = MyTaskSet
