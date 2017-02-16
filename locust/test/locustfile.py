6d3fc2bb540d4d2153e9e619890cc6f552e45faa
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2017-02-17 00:11:52 +0100

    Improve logging test results to influxdb
    Extract more test result info from response directly after the call
    instead of using event hook upon success and failure

--- locust/test/locustfile.py
+++ locust/test/locustfile.py
@@ -5,6 +5,7 @@ import json
 import requests
 from influxdb import InfluxDBClient
 import os
+from requests.exceptions import ConnectionError
 
 class MyTaskSet(TaskSet):
 
@@ -13,120 +14,140 @@ class MyTaskSet(TaskSet):
 
     @task(1)
     def view(self):
-        self.client.get("/jsp/rss.jsp")
+        with self.client.get("/jsp/rss.jsp", catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def addAbc(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/abc.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/abc.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def addBbc(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/bbc.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/bbc.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def addCnn(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/cnn.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/cnn.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def addDw(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/dw.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/dw.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def addForbes(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/forbes.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/forbes.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def addReuters(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/reuters.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/reuters.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def addSpiegel(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/spiegel.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/spiegel.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def addWsj(self):
-        self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/wsj.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"url":"http://rssserver/wsj.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def deleteAbc(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/abc.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/abc.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def deleteBbc(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/bbc.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/bbc.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def deleteCnn(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/cnn.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/cnn.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def deleteDw(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/dw.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/dw.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def deleteForbes(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/forbes.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/forbes.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def deleteReuters(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/reuters.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/reuters.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def deleteSpiegel(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/spiegel.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/spiegel.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
 
     @task(1)
     def deleteWsj(self):
-        self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/wsj.xml", "username":self.user_id})
+        with self.client.post("/jsp/rss.jsp", {"delFeedUrl":"http://rssserver/wsj.xml", "username":self.user_id}, catch_response=True) as response:
+            self.log_response(response)
+
+    def log_response(self, response):
+        json_body = [
+                {
+                    "measurement": "test_results",
+                    "tags": {
+                        "status_code": response.status_code,
+                        "reason": response.reason,
+                        "url": response.request.url,
+                        "path_url": response.request.path_url,
+                        "method": response.request.method,
+                        "body": response.request.body
+                        },
+                    "fields": {
+                        "status_code": response.status_code,
+                        "reason": response.reason,
+                        "elapsed": response.elapsed.total_seconds(),
+                        "url": response.request.url,
+                        "path_url": response.request.path_url,
+                        "method": response.request.method,
+                        "body": response.request.body
+                        }
+                    }
+                ]
+        InfluxDBWriter.write(json_body)
 
 class MyLocust(HttpLocust):
     task_set = MyTaskSet
     min_wait = 5000
     max_wait = 10000
 
-def on_request_success(request_type, name, response_time, response_length, **kw):
-    json_body = [
-            {
-                "measurement": "test_results",
-                "tags": {
-                    "request_type": request_type,
-                    "name": name,
-                    "exception": ""
-                    },
-                "fields": {
-                    "request_type": request_type,
-                    "name": name,
-                    "response_time": response_time,
-                    "response_length": response_length,
-                    "exception": ""
-                    }
-                }
-            ]
-    client.write_points(json_body)
+class InfluxDBWriter():
+    connected = False
+    influxdb_url = os.environ['INFLUXDB_URL']
+    influxdb_port = os.environ['INFLUXDB_PORT']
+    client = None
 
-def on_request_failure(request_type, name, response_time, exception, **kw):
-    json_body = [
-            {
-                "measurement": "test_results",
-                "tags": {
-                    "request_type": request_type,
-                    "name": name,
-                    "exception": str(exception)
-                    },
-                "fields": {
-                    "request_type": request_type,
-                    "name": name,
-                    "response_time": response_time,
-                    "response_length": 0,
-                    "exception": str(exception)
-                    }
-                }
-            ]
-    client.write_points(json_body)
+    @staticmethod
+    def connect():
+        print("Connecting to InfluxDB")
+        InfluxDBWriter.client = InfluxDBClient(InfluxDBWriter.influxdb_url, InfluxDBWriter.influxdb_port, 'root', 'root', 'locust')
+        InfluxDBWriter.client.create_database('locust')
+        InfluxDBWriter.connected = True
+        print("Connected to InfluxDB")
 
-influxdb_url = os.environ['INFLUXDB_URL']
-influxdb_port = os.environ['INFLUXDB_PORT']
-events.request_success += on_request_success
-events.request_failure += on_request_failure
-client = InfluxDBClient(influxdb_url, influxdb_port, 'root', 'root', 'locust')
-client.create_database('locust')
+    @staticmethod
+    def write(json_body):
+        try:
+            if (InfluxDBWriter.connected == False):
+                InfluxDBWriter.connect()
+            InfluxDBWriter.client.write_points(json_body)
+        except ConnectionError as e:
+            InfluxDBWriter.connected = False
+            print("ERROR: Cannot connect to InfluxDB. Dropping data point. See exception below for details")
+            print(e)
